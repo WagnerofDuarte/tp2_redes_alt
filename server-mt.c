5e3e5a1c28518882c463afebfaccd36e5011f762
Wagner Duarte <46401074+WagnerofDuarte@users.noreply.github.com>
2025-06-29 20:20:45 -0300

    d

--- server-mt.c
+++ server-mt.c
@@ -211,7 +211,7 @@ void *game_thread(void *arg) {
             // broadcast_message(multiplier_msg);
             pthread_mutex_lock(&players_mutex);
             for (int i = 0; i < MAX_PLAYERS; i++) {
-                if (players[i].sock != 0 && !players[i].has_cashed_out) {
+                if (players[i].sock != 0 && !players[i].has_cashed_out && players[i].has_bet) {
                     send_message_to_client(players[i].sock, multiplier_msg);
                 }
             }
